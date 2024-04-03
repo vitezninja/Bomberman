9002d0e3e567d44d67782e057390f9a72fe840b5
Fodor Attila <l23j3q@inf.elte.hu>
2024-04-03 23:23:28 +0200

    Bug fix

--- Scripts/Menus/infoMenu.gd
+++ Scripts/Menus/infoMenu.gd
@@ -1,10 +1,14 @@
-extends Node2D
+class_name InfoMenu
+extends Control
 
-@onready var mainMenu = preload("res://Scenes/Menus/MainMenu.tscn")
+@onready var back_button: Button = %BackButton
+@onready var quit_button: Button = %QuitButton
+const MAIN_MENU_TEST = preload("res://Scenes/Menus/MainMenu.tscn")
 
-func _on_quit_pressed():
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
-func _on_back_pressed():
-	get_tree().get_first_node_in_group("Menu").add_child(mainMenu.instantiate())
+func _on_back_button_pressed() -> void:
+	var main = MAIN_MENU_TEST.instantiate()
+	get_tree().get_first_node_in_group("Menu").add_child(main)
 	queue_free()
