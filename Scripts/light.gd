6a93705d9eb751fc1bd7812cc1782d3f172e184b
Tóth Benedek Vitéz <tothvitez@gmail.com>
2024-06-18 23:29:21 +0200

    Added light to explosions and powerups

--- Scripts/light.gd
+++ Scripts/light.gd
@@ -4,17 +4,16 @@ extends Node2D
 @onready var shadow: Light2D = %Shadow
 
 @export var Enabled: bool = false
-@export_range(1, 20) var radius: float = 1.0
-@export_range(1, 10) var light_strenght: float = 1.0
-
-
-func _ready() -> void:
-	light.texture_scale = radius
-	shadow.texture_scale = radius
-	
-	light.energy = light_strenght
+@export_range(0, 20) var radius: float = 1.0
+@export_range(0, 10) var light_strenght: float = 1.0
 
 
 func _physics_process(_delta: float) -> void:
 	light.enabled = Enabled
 	shadow.enabled = Enabled
+	
+	light.texture_scale = radius
+	shadow.texture_scale = radius
+	
+	light.energy = light_strenght
+	shadow.energy = light_strenght / 2
